c4c30b16c37a00109523bf2f6a3a6834315a3c83
Mohamed Anwar <mohamedelfeky777@gmail.com>
2024-05-05 23:32:17 +0300

    Add DB models, config,main, and repositories functions

---
+++ config.py
@@ -0,0 +1,11 @@
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import database_exists, create_database
+
+engine = create_engine("postgresql://postgres:132000@localhost:5432/testSQLalchemy2", echo=True)
+if not database_exists(engine.url):
+    create_database(engine.url)
+
+base = declarative_base()
+print(database_exists(engine.url))                 
